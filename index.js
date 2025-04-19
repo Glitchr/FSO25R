b5fa5c99d0f0362b27e5c7c02f93a16875f978e9
Manuel Maldonado <19945996+Glitchr@users.noreply.github.com>
2025-04-19 11:48:04 -0400

    Add initial HTML and SVG files, update server to manage notes instead of persons

--- index.js
+++ index.js
@@ -1,97 +1,97 @@
 const express = require("express");
-const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpont" });
+};
+
+app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
-app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
-);
+app.use(requestLogger);
 
-morgan.token("body", (req) => JSON.stringify(req.body));
-
-let persons = [
+let notes = [
   {
     id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
+    content: "HTML is easy",
+    important: true,
   },
   {
     id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
+    content: "Browser can execute only JavaScript",
+    important: false,
   },
   {
     id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
   },
 ];
 
-app.get("/", (req, res) => {
-  res.send("<h1>Phonebook</h1>");
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/info", (req, res) => {
-  const date = new Date();
-  const personCount = persons.length;
-  res.send(`<p>Phonebook has info for ${personCount} people</p><p>${date}</p>`);
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
 });
 
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
-});
-
-app.get("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    res.json(person);
+app.get("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    response.json(note);
   } else {
-    res.status(404).json({ error: "Person not found" });
+    response.statusMessage = "Note not found";
+    response.status(404).end();
   }
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
-  persons = persons.filter((person) => person.id !== id);
+app.delete("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter((note) => note.id !== id);
 
-  res.status(204).end();
+  response.status(204).end();
 });
 
-app.post("/api/persons", (req, res) => {
-  const body = req.body;
-  const newId = Math.floor(Math.random() * 1000000).toString();
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((note) => Number(note.id))) : 0;
+  return String(maxId + 1);
+};
 
-  if (!body.name || !body.number) {
-    return res.status(400).json({
-      error: "Name or number is missing",
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
     });
   }
 
-  if (persons.some((person) => person.name === body.name)) {
-    return res.status(400).json({
-      error: "Name must be unique",
-    });
-  }
-
-  const person = {
-    name: body.name,
-    number: body.number,
-    id: newId,
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
   };
 
-  persons = persons.concat(person);
+  notes = notes.concat(note);
 
-  res.json(person);
+  response.json(note);
 });
 
-const PORT = 3001;
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log("Server running on http://localhost:3001");
+  console.log(`Server running on port http://localhost:${PORT}`);
 });
